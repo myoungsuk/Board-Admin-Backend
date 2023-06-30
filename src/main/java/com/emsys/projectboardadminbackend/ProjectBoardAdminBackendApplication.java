4085bc6977654b5e8485368f95b188b1e8961392
강명석 <k4858678@gmail.com>
2023-07-01 02:51:18 +0900

    start

---
+++ src/main/java/com/emsys/projectboardadminbackend/ProjectBoardAdminBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emsys.projectboardadminbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBoardAdminBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBoardAdminBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emsys/projectboardadminbackend/ProjectBoardAdminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsys.projectboardadminbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBoardAdminBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
